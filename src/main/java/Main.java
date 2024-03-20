c3f58ce0df28af4fa1bbaa920779a82595a31b81
KazmirzXD <161707953+KazmirzXD@users.noreply.github.com>
2024-03-20 13:18:59 +0000

    2_1
    hello word w pionie

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,8 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
-
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    String str = "Hello world!";
+    for (int i = 0; i < str.length(); i++) {
+      System.out.println(str.charAt(i));
+    }
   }
-
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
 }
